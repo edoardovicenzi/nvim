a45aa2b77fcce5f77191a0ec5849f7775fe75ff7
edoardovicenzi <evicenzi357@gmail.com>
2024-02-10 01:42:53 +0100

    Added Lualine and Mind

--- lua/Edoardo/packer.lua
+++ lua/Edoardo/packer.lua
@@ -16,33 +16,36 @@ return require('packer').startup(function(use)
         as = 'rose-pine',
         config = function() vim.cmd('colorscheme rose-pine') end })
 
-        use (
+    use (
         'nvim-treesitter/nvim-treesitter',
         {run = ':TSUpdate'}
-        )
-        use ('MunifTanjim/prettier.nvim')
-        use ('jose-elias-alvarez/null-ls.nvim')
-        use 'windwp/nvim-ts-autotag'
-        use 'windwp/nvim-autopairs'
-        use ('ThePrimeagen/harpoon')
-        use ('mbbill/undotree')
-        use ('tpope/vim-fugitive')
-        use {
-            'VonHeikemen/lsp-zero.nvim',
-            branch = 'v3.x',
-            requires = {
-                --- Uncomment these if you want to manage LSP servers from neovim
-                {'williamboman/mason.nvim'},
-                {'williamboman/mason-lspconfig.nvim'},
+    )
+    use ('MunifTanjim/prettier.nvim')
+    use ('jose-elias-alvarez/null-ls.nvim')
+    use 'windwp/nvim-ts-autotag'
+    use 'windwp/nvim-autopairs'
+    use ('ThePrimeagen/harpoon')
+    use ('mbbill/undotree')
+    use ('tpope/vim-fugitive')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-                -- LSP Support
-                {'neovim/nvim-lspconfig'},
-                -- Autocompletion
-                {'hrsh7th/nvim-cmp'},
-                {'hrsh7th/cmp-nvim-lsp'},
-                {'L3MON4D3/LuaSnip'},
-            }
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
         }
+    }
+    use {
+        'nvim-lualine/lualine.nvim'
+    }
     use {
         'Selyss/mind.nvim',
         branch = 'v2.2',
@@ -54,5 +57,4 @@ return require('packer').startup(function(use)
             require'mind'.setup()
         end
     }
-    require'nvim-web-devicons'.setup{}
-    end)
+end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -94,6 +94,11 @@ _G.packer_plugins = {
     path = "/Users/edo/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/Users/edo/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
     path = "/Users/edo/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
@@ -184,14 +189,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
 -- Config for: mind.nvim
 time([[Config for mind.nvim]], true)
 try_loadstring("\27LJ\2\n2\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\tmind\frequire\0", "config", "mind.nvim")
 time([[Config for mind.nvim]], false)
+-- Config for: rose-pine
+time([[Config for rose-pine]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
+time([[Config for rose-pine]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
